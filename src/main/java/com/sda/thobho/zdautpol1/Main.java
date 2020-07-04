289a31977b87254987d35c2356513bfa7aa658e1
Tomek <tomasz.bar3@gmail.com>
2020-07-04 12:38:47 +0200

    first commit

---
+++ src/main/java/com/sda/thobho/zdautpol1/Main.java
@@ -0,0 +1,10 @@
+package com.sda.thobho.zdautpol1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tests!");
+    }
+
+
+}
---
+++ src/test/java/com/sda/thobho/zdautpol1/MyFirstJUnitTest.java
@@ -0,0 +1,13 @@
+package com.sda.thobho.zdautpol1;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstJUnitTest {
+
+
+    @Test
+    public void myTest(){
+
+    }
+
+}
